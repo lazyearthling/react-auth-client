6a4cb4e89faa3ac98d7dfc390c6939bc6de8417b
ruksad muhammed <ruksad muhammed>
2018-02-17 22:03:34 +0530

    add default middleware setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import reducers from './reducers';
 
 import App from './components/app';
 
+//Add middleware
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
       <App />
     </Provider>
     ,document.querySelector('.container'));
