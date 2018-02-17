353022de8d1a9d4914f370dfefa4aa8251ca384d
ruksad muhammed <ruksad muhammed>
2018-02-17 21:53:09 +0530

    add reducer setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
+import reducers from './reducers';
+
 import App from './components/app';
 
 ReactDOM.render(
-    <Provider store={createStore(()=>{})}>
+    <Provider store={createStore(reducers)}>
       <App />
     </Provider>
     ,document.querySelector('.container'));
