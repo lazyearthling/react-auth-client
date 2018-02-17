ba96b716a6381721906b1d34fc5e51a5d73c0609
ruksad muhammed <ruksad muhammed>
2018-02-18 00:04:20 +0530

    install react-router-dom and use it for routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import {Route,Switch,BrowserRouter} from 'react-router-dom';
 
 import reducers from './reducers';
 
@@ -12,6 +13,12 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={App} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
     ,document.querySelector('.container'));
