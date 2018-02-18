ae9cd591bda8a7afd80eb48dbdb5d207b26ddfd6
ruksad muhammed <ruksad muhammed>
2018-02-18 17:12:29 +0530

    add header props with functional component

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,35 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 class Header extends Component{
+
+  renderLinks(){
+    if(this.props.authenticated)
+      return(
+        <li className="nav-item"><Link to="/signout"  className="nav-link">Sign Out</Link></li>
+      );
+    else return [
+      <li className="nav-item" key={1}><Link to="/signin"  className="nav-link">Sign In</Link></li>,
+      <li className="nav-item" key={2}><Link to="/signup"  className="nav-link">Sign Up</Link></li>
+    ]
+  }
+
   render(){
     return(
 
       <nav className="navbar navbar-light" style={{"backgroundColor": "#e3f2fd"}}>
         <Link to ='/' className="navbar-brand">Brand Name</Link>
         <ul className="nav justify-content-end">
-          <li className="nav-item"><Link to="/signin"  className="nav-link">Sign In</Link></li>
-          <li className="nav-item"><Link to="/signup"  className="nav-link">Sign Up</Link></li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return {
+    authenticated:state.auth.authenticated
+  }
+}
+
+export default connect(mapStateToProps)(Header);
